080787d5e2588218ba570b6ccc38f529d4c47e77
Victor Moysen <vicmoysen@hotmail.com>
2024-01-11 13:32:53 -0600

    Proyecto base Spring

---
+++ ColectivcCuatroBackEnd/src/test/java/com/colectivocuatro/ColectivcCuatroBackEnd/ColectivcCuatroBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colectivocuatro.ColectivcCuatroBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColectivcCuatroBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
